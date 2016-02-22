f20ec95c234c61c038b7a85db471f1c898b8e687
wangxiaohui <wangxiaohui@souche.com>
2016-02-22 16:00:22 +0800

    init tabbar
    init tartar

---
+++ TabbarDemo/TabbarDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TabbarDemo
+//
+//  Created by souche on 16/2/22.
+//  Copyright © 2016年 wxhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TabbarDemo/TabbarDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TabbarDemo
+//
+//  Created by souche on 16/2/22.
+//  Copyright © 2016年 wxhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
